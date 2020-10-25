39dc056f64c796e029326e7ef6bcd2733810a45f
hansenfp <61291503+hansenfp@users.noreply.github.com>
2020-10-25 21:16:44 +0100

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,81 @@
+Question 1
+
+var soda = "Pepsi";
+
+Question 2
+
+var person = {
+    country:"norway",
+    valuta:"nor"
+};
+
+Question 3
+
+var outOfStock = true;
+if (outOfStock === true) {
+    console.log("Out of stock");
+} else {
+    console.log("In stock");
+
+Question 4
+
+var numberArray = [1, 2, 3, 4, 5];
+
+for (var i = 0; i < numberArray.length; i++) {
+    console.log(numberArray[i]);
+}
+
+Question 5
+
+for (var i = 15; i <= 25; i++) {
+    console.log(i);
+}
+
+Question 6
+
+for (var i = 15; i <= 25; i++) {
+    if (i === 20) {
+        console.log(i);
+    } 
+}
+
+Question 7
+
+var shop [
+    {
+        id: food,
+        age: 0,
+        forAll: true
+    }
+    {
+        id: cigarettes,
+        age: 18,
+        forAll: false
+    }
+];
+
+
+Question 8
+
+function whatIDontLike(badVibes) {
+    console.log("I dont like " + badVibes);
+
+    whatIDontLike("People that dont smile");
+}
+
+
+Question 9
+
+function vehicle(boat, car) {
+    var sum = 4 - 2;
+    console.log("I want a new car");
+}
+
+Question 10
+
+var someArray = [];
+
+function addToArray(value) {
+}
+
+addToArray("Finish!");
